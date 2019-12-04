70c3e3a31857ce78935f6482fb82dcc4ddff5538
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-12-04 11:16:48 +0800

    ch5

---
+++ src/ch5/joinMap/joinMap.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <boost/format.hpp>  // for formating strings
+#include <sophus/se3.hpp>
+#include <pangolin/pangolin.h>
+//#include <pcl/point_types.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+
+using namespace std;
+typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
+
+// 在pangolin中画图，已写好，无需调整
+void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud);
+
+int main(int argc, char **argv) {
+    vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
+    TrajectoryType poses;         // 相机位姿
+
+    ifstream fin("../pose.txt");
+    if (!fin) {
+        cerr << "请在有pose.txt的目录下运行此程序" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        boost::format fmt("../%s/%d.%s"); //图像文件格式
+        colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
+        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
+
+        double data[7] = {0};
+        for (auto &d:data) {
+            fin >> d;
+        }
+        Sophus::SE3d pose(Eigen::Quaterniond(data[6], data[3], data[4], data[5]),
+                          Eigen::Vector3d(data[0], data[1], data[2]));
+        poses.push_back(pose);
+    }
+
+    // 计算点云并拼接
+    // 相机内参 
+    double cx = 325.5;
+    double cy = 253.5;
+    double fx = 518.0;
+    double fy = 519.0;
+    double depthScale = 1000.0;
+    vector<Vector6d, Eigen::aligned_allocator<Vector6d>> pointcloud;
+    pointcloud.reserve(1000000);
+
+    // 定义点云使用的格式：这里用的是XYZRGB
+//    typedef pcl::PointXYZRGB PointT;
+//    typedef pcl::PointCloud<PointT> PointCloud;
+    // 新建一个点云
+//    PointCloud::Ptr pointCloud(new PointCloud);
+
+    for (int i = 0; i < 5; i++) {
+        cout << "转换图像中: " << i + 1 << endl;
+        cv::Mat color = colorImgs[i];
+        cv::Mat depth = depthImgs[i];
+        Sophus::SE3d T = poses[i];
+        // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
+        for (int v = 0; v < color.rows; v+=2)
+            for (int u = 0; u < color.cols; u+=2) {
+                unsigned int d = depth.ptr<unsigned short>(v)[u]; // 深度值
+                if (d == 0) continue; // 为0表示没有测量到
+                Eigen::Vector3d point;
+                point[2] = double(d) / depthScale;
+                point[0] = (u - cx) * point[2] / fx;
+                point[1] = (v - cy) * point[2] / fy;
+                Eigen::Vector3d pointWorld = T * point;
+
+                Vector6d p;
+                p.head<3>() = pointWorld;
+                p[5] = color.data[v * color.step + u * color.channels()];   // blue
+                p[4] = color.data[v * color.step + u * color.channels() + 1]; // green
+                p[3] = color.data[v * color.step + u * color.channels() + 2]; // red
+                pointcloud.push_back(p);
+//                cout << color.step << ", " << color.channels() << endl; // 1920, 3
+//                cout << color.rows << ", " << color.cols << endl; // 480, 640
+
+//                PointT p2;
+//                p2.x = pointWorld[0];
+//                p2.y = pointWorld[1];
+//                p2.z = pointWorld[2];
+//                p2.b = color.data[v * color.step + u * color.channels()];
+//                p2.g = color.data[v * color.step + u * color.channels() + 1];
+//                p2.r = color.data[v * color.step + u * color.channels() + 2];
+//                pointCloud->points.push_back(p2);
+            }
+    }
+
+    cout << "点云共有" << pointcloud.size() << "个点." << endl;
+    showPointCloud(pointcloud);
+
+//    pointCloud->is_dense = false;
+//    cout << "点云共有" << pointCloud->size() << "个点." << endl;
+//    pcl::io::savePCDFileBinary("map.pcd", *pointCloud);
+
+    // 方便起见这里使用指针的形式
+//    PointCloud::Ptr cloud(new PointCloud);
+//    if (pcl::io::loadPCDFile("map.pcd", *cloud) == -1) {
+//        PCL_ERROR("can not read file");
+//        return 0;
+//    }
+
+    // 展示 map.pcd 中的内容
+//    pcl::visualization::PCLVisualizer viewer;
+//    viewer.addPointCloud(cloud, "cloud");
+//    viewer.spin();
+
+    return 0;
+}
+
+void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud) {
+
+    if (pointcloud.empty()) {
+        cerr << "Point cloud is empty!" << endl;
+        return;
+    }
+
+    pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+            pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
+
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        for (auto &p: pointcloud) {
+            glColor3d(p[3] / 255.0, p[4] / 255.0, p[5] / 255.0);
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
+    return;
+}
